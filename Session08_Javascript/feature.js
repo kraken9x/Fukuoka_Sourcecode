87b0a52c45528f8cd5a5176373264324a465cd82
kraken9x <47894224+kraken9x@users.noreply.github.com>
2024-09-16 14:06:59 +0700

    Add files via upload
    upload

---
+++ Session08_Javascript/feature.js
@@ -0,0 +1,68 @@
+// alert("Hello world !!!");
+
+// Khai báo biến (Variable Declaration)
+// var box = 10;
+
+// Dòng code Javascript 1
+// Dòng code Javascript 2
+// Dòng code Javascript 3
+// Dòng code Javascript 4
+// Dòng code Javascript 5
+// .....
+// Dòng code Javascript 100
+// Sử dụng biến
+// box;
+// alert(box);
+// .....
+// Dòng code Javascript 200
+// box = 20;
+// alert(box);
+// ....
+// Dòng code JAavascript N
+
+//
+// var firstNumber = 10;
+
+// let secondNumber = 20;
+
+// const thirdNumber = 30;
+
+// console.log(thirdNumber);
+
+// let year = prompt("Mời bạn nhập năm sinh:"); // 1995
+
+// console.log(year);
+
+// Ex1:
+// Viết chương trình tính tổng 2 số
+// Cho người dùng nhập vào số thứ nhất
+// Cho người dùng nhập vào số thứ 2
+// Tính tổng 2 số
+// In tổng đó ra màn hình console
+
+// Input: firstNumber, secondNumber
+
+// Process:
+// // B1: Tạo 1 biến firstNumber lưu trữ lại giá trị người dùng nhập vào
+// let firstNumber = +prompt("Mời bạn nhập vào số a"); // 10 (string)
+// // B2: Tạo 1 biến secondNumber lưu trữ lại giá trị người dùng nhập vào
+// let secondNumber = +prompt("Mời bạn nhập vào số b"); // 20 (string)
+// // B3: Tiến hành cộng firstNumber + secondNumber
+// let sum = firstNumber + secondNumber; // 10 + 20 ---> 1020
+// // B4: In tổng đó ra màn hình
+// console.log(sum);
+// // Output: Tổng firstNumber + secondNumber
+
+// let a = 11;
+// let b = 3;
+
+// console.log(a % b);
+
+let school = "Rikkei Academy";
+let age = 19;
+let sentence = "is awesome!";
+
+console.log(school);
+console.log(age);
+
+console.log(school + " " + sentence); // Rikkei Academy is awesome
